eee8afa837b7f586b2b9b298f3489e0e9cefc46f
bipin gosain <bipingosain3492@gmail.com>
2019-04-21 18:01:33 +0530

    Flyweight first commit

---
+++ Assets/ShapeFactory.cs
@@ -0,0 +1,152 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public enum Shapes{ SPHERE, CUBE, CYLINDER}
+public enum Sizes{ SMALL, MEDIUM, LARGE}
+/// <summary>
+/// The 'Flyweight' interface
+/// </summary>
+public abstract class AbstractShape: MonoBehaviour
+{
+    public Shapes Shape;
+    public Sizes Size;
+    public Color ColorOfShape;
+}
+
+class CubeShape : AbstractShape
+{
+    public override string ToString(){
+
+        return string.Format( "Shape= {0}, Color= {1}, Size= {2}", this.Shape, this.ColorOfShape, this.Size );
+    }
+}
+
+class SphereShape : AbstractShape
+{
+    public override string ToString(){
+
+        return string.Format( "Shape= {0}, Color= {1}, Size= {2}", this.Shape, this.ColorOfShape, this.Size );
+    }
+}
+
+class CylinderShape: AbstractShape{
+
+    public override string ToString(){
+
+        return string.Format( "Shape= {0}, Color= {1}, Size= {2}", this.Shape, this.ColorOfShape, this.Size );
+    }
+}
+
+
+
+public class ShapeFactory : MonoBehaviour{
+    
+    int currentIndex = 0;
+    Queue<AbstractShape> shapes = new Queue<AbstractShape>();
+ 
+    public int TotalObjectsCreated
+    {
+       get { return shapes.Count; }
+    }
+
+    public GameObject Dequeue(){
+
+        return this.shapes.ElementAt(this.currentIndex++).gameObject;
+    }
+
+    public void RepositionQueue(List<Vector3> positions){
+        
+        int index = 0;
+        for (int i = this.currentIndex; i < this.currentIndex + 2; i++)
+            this.shapes.ElementAt(i).gameObject.transform.position = positions[index++];
+        
+    }
+ 
+    void SetObject(GameObject toSet, Shapes shape, Sizes size, Color color){
+
+        toSet.AddComponent<BoxCollider>();
+        toSet.AddComponent<Rigidbody>();
+        toSet.GetComponent<Rigidbody>().isKinematic = true;
+        switch(shape){
+            case Shapes.CUBE:
+                toSet.GetComponent<CubeShape>().Shape = shape;
+                toSet.GetComponent<CubeShape>().ColorOfShape = color;
+                toSet.GetComponent<CubeShape>().Size = size;
+                break;
+            case Shapes.CYLINDER:
+                toSet.GetComponent<CylinderShape>().Shape = shape;
+                toSet.GetComponent<CylinderShape>().ColorOfShape = color;
+                toSet.GetComponent<CylinderShape>().Size = size;
+                break;
+            case Shapes.SPHERE:
+                toSet.GetComponent<SphereShape>().Shape = shape;
+                toSet.GetComponent<SphereShape>().ColorOfShape = color;
+                toSet.GetComponent<SphereShape>().Size = size;break;
+        }
+        toSet.GetComponent<MeshRenderer>().materials[0].color = color;
+        
+        switch(size){
+            case Sizes.SMALL:
+            toSet.transform.localScale = new Vector3(.3f, .3f, .3f);  
+            break;
+            case Sizes.MEDIUM:
+            toSet.transform.localScale = new Vector3(.6f, .6f, .6f);
+            break;
+            case Sizes.LARGE:
+            toSet.transform.localScale = new Vector3(.9f, .9f, .9f);
+            break;
+        }
+    }
+    public GameObject GetShape(Shapes cShape, Sizes cSize, Color cColor, out bool created)
+    {
+        GameObject shape = null;
+        if (shapes.Any(x=> x.Shape == cShape && x.ColorOfShape == cColor && x.Size == cSize )){
+            
+            created = false;
+            for (int i = 0; i < this.shapes.Count; i++)
+            {
+                if(this.shapes.ElementAt(i).Shape == cShape && this.shapes.ElementAt(i).ColorOfShape == cColor && this.shapes.ElementAt(i).Size == cSize ){
+                    shape = this.shapes.ElementAt(i).gameObject;
+                    break;
+                }
+            }
+        }
+        else
+        {
+            created = true;
+            switch (cShape)
+            {
+                case Shapes.CUBE:
+                shape = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                shape.AddComponent<CubeShape>();
+                this.SetObject(shape, cShape, cSize, cColor);
+
+
+                this.shapes.Enqueue(shape.GetComponent<CubeShape>());
+                break;
+
+                case Shapes.SPHERE:
+                shape= GameObject.CreatePrimitive(PrimitiveType.Sphere);
+                shape.AddComponent<SphereShape>();
+                this.SetObject(shape, cShape, cSize, cColor);
+                this.shapes.Enqueue(shape.GetComponent<SphereShape>());
+                break;
+
+                case Shapes.CYLINDER:
+                shape = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
+                shape.AddComponent<CylinderShape>();
+                this.SetObject(shape, cShape, cSize, cColor);
+                this.shapes.Enqueue(shape.GetComponent<CylinderShape>());
+
+                break;
+                
+                default:
+                    throw new System.Exception("Not implemented for this type!");
+            }
+            
+        }
+        return shape;
+    }
+}
---
+++ Assets/UIController.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Linq;
+
+public class UIController : MonoBehaviour
+{
+    List<Vector3> createPositions = new List<Vector3>();
+    [SerializeField]
+    GameObject container;
+    [SerializeField]
+    Button createObject;
+    [SerializeField]
+    GameObject selectionPanel;
+    [SerializeField]
+    ToggleGroup shape, color, size ;
+    ShapeFactory shapeFac = new ShapeFactory();
+    
+    
+    Shapes currentShape;
+    Sizes currentSize;
+    Color currentColor;
+    
+    void Start()
+    {
+        createPositions.Add(new Vector3(-2, 2, 0));
+        createPositions.Add(new Vector3(2, 2, 0));
+
+        createObject.onClick.AddListener(SelectObject);
+    }
+
+    void SelectObject(){
+
+        List<Toggle> selectedShape = shape.ActiveToggles().ToList<Toggle>();
+        List<Toggle> selectedColor = color.ActiveToggles().ToList<Toggle>();
+        List<Toggle> selectedSize = size.ActiveToggles().ToList<Toggle>();
+
+        selectionPanel.SetActive(false);
+        
+        this.currentShape = (Shapes)System.Enum.Parse(typeof(Shapes), selectedShape[0].name.ToUpper());
+        this.currentSize = (Sizes)System.Enum.Parse(typeof(Sizes), selectedSize[0].name.ToUpper());
+
+        switch(selectedColor[0].name){
+            case "RED":
+            this.currentColor = Color.red;
+            break;
+            case "GREEN":
+            this.currentColor = Color.green;
+            break;
+            case "BLUE":
+            this.currentColor = Color.blue;
+            break;
+        }
+        
+
+        bool createdNew = false;
+        GameObject desiredObject =  this.shapeFac.GetShape(this.currentShape, this.currentSize, this.currentColor, out createdNew);   
+        
+        
+
+        if(createdNew){
+
+            if(this.shapeFac.TotalObjectsCreated <= 2)
+                desiredObject.transform.position = this.createPositions[this.shapeFac.TotalObjectsCreated - 1];
+            else{
+                
+                GameObject top = this.shapeFac.Dequeue();      
+                top.GetComponent<Rigidbody>().isKinematic = false;
+                Vector3 pos = this.container.transform.position;
+                top.transform.position =  new Vector3(UnityEngine.Random.Range(-2.5f, 2.5f), pos.y + 2, pos.z - .5f);
+                this.shapeFac.RepositionQueue(this.createPositions);
+            }
+        }
+        
+        Debug.Log("Is a new Object Created: " + createdNew);
+        Debug.Log(string.Format("Retrieved Object = {0}", desiredObject));
+        Debug.Log(string.Format("Total Objects Created = {0}", this.shapeFac.TotalObjectsCreated));
+    }
+}
